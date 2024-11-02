5db92c5e05622c8a37bc5a2425fbd4ed51fe8759
AnA <analuciamrls@gmail.com>
2024-11-01 22:00:34 -0300

    agrego los models

---
+++ TP5/Models/Presupuesto.cs
@@ -0,0 +1,20 @@
+﻿namespace TP5.Models
+{
+    public class Presupuesto
+    {
+        private int idPresupuesto;
+        private string nombreDestinatario;
+        List<PresupuestoDetalle> detalle;
+
+        public Presupuesto(int idPresupuesto, string nombreDestinatario)
+        {
+            this.idPresupuesto = idPresupuesto;
+            this.nombreDestinatario = nombreDestinatario;
+            this.detalle = new();
+        }
+
+        public int IdPresupuesto { get => idPresupuesto; set => idPresupuesto = value; }
+        public string NombreDestinatario { get => nombreDestinatario; set => nombreDestinatario = value; }
+        public List<PresupuestoDetalle> Detalle { get => detalle; set => detalle = value; }
+    }
+}
